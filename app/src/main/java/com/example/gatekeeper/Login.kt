7c44bd7a538997979dcbfd629dac6d212644ab65
SANKETH-CHANDRA-SHETTY <shettysankethshetty1@gmail.com>
2026-02-10 14:51:07 +0530

    add screen change functionality to sign up

---
+++ app/src/main/java/com/example/gatekeeper/Login.kt
@@ -0,0 +1,34 @@
+package com.example.gatekeeper
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.gatekeeper.databinding.ActivityLoginBinding
+import android.content.Intent
+
+
+class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.loginSignUp.setOnClickListener{
+            val intent=Intent(this,SignUp::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
